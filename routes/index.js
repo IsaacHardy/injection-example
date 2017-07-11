ac1ea4d7a0c3c8dbe23ce4f9173ff4327f090a4d
Isaac Hardy <isaac@ihardy.co>
2017-07-11 11:04:19 -0400

    added safe endpoint

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,22 @@ router.get('/', function(req, res) {
 });
 
 router.get('/mutants/:role', function(req, res, next){
+  // ') UNION SELECT usename||'_'||password FROM mutants --
   models.sequelize.query(
     "SELECT usename FROM mutants WHERE role like ('%" + req.params.role + "%')",
+    {type: sequelize.QueryTypes.SELECT})
+  .spread(function(data) {
+    res.json(data);
+  }).catch(function(err) {
+    res.send(err);
+  });
+});
+
+router.get('/mutants/safe/:role', function(req, res, next){
+  var dbEscape = models.sequelize.escape(req.params.role);
+
+  models.sequelize.query(
+    "SELECT usename FROM mutants WHERE role like ('%" + dbEscape.replace(/['"]+/g, '') + "%')",
     {type: sequelize.QueryTypes.SELECT})
   .spread(function(data) {
     res.json(data);
